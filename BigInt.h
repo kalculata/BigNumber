5abd857406fffdc78d9471fce557b6475316e597
kalculata <nprincehuzaifa@gmail.com>
2022-08-22 09:27:37 +0200

    BigNumberException class

--- BigInt.h
+++ BigInt.h
@@ -6,6 +6,17 @@
 
 typedef BigInt bigint;
 
+class BigNumberException: public std::exception {
+private:
+	std::string message;
+public:
+	BigNumberException(std::string msg): message(msg) {}
+	
+	std::string what() {
+		return message;
+	}
+};
+
 class BigInt {
 private:
 	std::string val;
@@ -13,5 +24,29 @@ private:
 public:
 	BigInt() {
 		val = "0";
+	}
+
+	BigInt(std::string value) {
+		val = validate(value);
+	}
+
+	BigInt(const char* value) {
+		val = validate(value);
+	}
+
+	BigInt(int value) {
+		val = std::to_string(value);
+	}
+private:
+	std::string validate(std::string value) {
+		if(std::regex_match(value, std::regex("[-\+]?[0-9]+"))) {
+			if (val[0] == '+') {
+				return value.erase(0, 1);
+			}
+			return value;
+		}
+		else {
+			throw BigNumberException("Invalid number");
+		}
 	}
 };
